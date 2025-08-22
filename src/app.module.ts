f7be80fab598a3aee00be51b51587db60643ff2e
AliHaidar322 <alibinkhalid322@gmail.com>
2025-08-22 15:05:28 +0500

    completed lesson 5

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeeModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
